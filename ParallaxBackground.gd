f1b285b8e74f2dd18d66f64fbc07f7a1b685794d
MaxiZambrana <maxizambrana14@gmail.com>
2023-04-29 22:41:24 -0300

    por ahora, nada

---
+++ ParallaxBackground.gd
@@ -0,0 +1,12 @@
+extends ParallaxBackground
+
+var velocidad = 12
+var velocity = Vector2()
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	velocity.x = velocidad
--- Scripts/Delivery_run/moto_delivery.gd
+++ Scripts/Delivery_run/moto_delivery.gd
@@ -3,26 +3,25 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
+#var velocidad_Arriba_Abajo = 10
+var velocidad_moto = 15
+var velocidad = Vector2()
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 
 func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
-
-	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+	if (Input.is_action_pressed("p1_moverArriba")):
+		velocidad.y = -velocidad_moto+10
+	elif (Input.is_action_pressed("p1_moverAbajo")):
+		velocidad.y = velocidad_moto-10
+	elif (Input.is_action_pressed("p1_moverIzquierda")):
+		velocidad.x = -velocidad_moto
+	elif (Input.is_action_pressed("p1_moverDerecha")):
+		velocidad.x = velocidad_moto
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
-	move_and_slide()
+		velocidad.y = 0
+		velocidad.x = 0
+	#velocidad_moto = velocidad_moto.normalized()*100
+	move_and_collide(velocidad)
