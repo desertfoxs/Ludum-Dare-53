a752304ae6648bf3606373d3358f8819dd6c6b65
MaxiZambrana <maxizambrana14@gmail.com>
2023-04-30 01:53:37 -0300

    no tengo idea que puse

--- Scripts/Delivery_run/moto_delivery.gd
+++ Scripts/Delivery_run/moto_delivery.gd
@@ -4,6 +4,7 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 #var velocidad_Arriba_Abajo = 10
+@export var aceleracion : int = 200
 var velocidad_moto = 15
 var velocidad = Vector2()
 
@@ -12,16 +13,16 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 
 func _physics_process(delta):
-	if (Input.is_action_pressed("p1_moverArriba")):
+	if (Input.is_action_pressed("ui_up")):
 		velocidad.y = -velocidad_moto+10
-	elif (Input.is_action_pressed("p1_moverAbajo")):
+	elif (Input.is_action_pressed("ui_down")):
 		velocidad.y = velocidad_moto-10
-	elif (Input.is_action_pressed("p1_moverIzquierda")):
+	elif (Input.is_action_pressed("ui_left")):
 		velocidad.x = -velocidad_moto
-	elif (Input.is_action_pressed("p1_moverDerecha")):
+	elif (Input.is_action_pressed("ui_right")):
 		velocidad.x = velocidad_moto
 	else:
 		velocidad.y = 0
 		velocidad.x = 0
-	#velocidad_moto = velocidad_moto.normalized()*100
-	move_and_collide(velocidad)
+	#velocidad_moto = velocidad_moto.normalized()*aceleracion
+	move_and_slide()
